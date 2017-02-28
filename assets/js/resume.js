c3142f78c708ac931fbac5a3307118145134c600
ztao <yushenxin@foxmail.com>
2017-02-28 21:34:30 +0800

    step6

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -10,10 +10,7 @@ $(function(){
 	/*存储临时数据*/
 	var resHtm=$resume.html();
 	var tpanHtm='<a class="next center-block">下一步</a>';
-	
-	/*时间控制*/
-	var time=1500;
-	
+		
 	/*步数记录*/
 	var step=1;
 		
@@ -24,26 +21,31 @@ $(function(){
 	/*点击看看*/
 	$(".yes").click(function(){
 		$tpp.hide(300).html("第一步，我们先建个空白的页面。").show(300);
-		$tpa.hide().html(tpanHtm);
-		$resume.html("<h1>我是空白页面</h1>").height("100vh");//生成空白页面
-		
-		//显示空白页
-		setTimeout(function(){
-			$typing.removeClass("col-md-offset-5 myhover");
-			$resume.removeClass("myhidden");
-			$tpa.show(200);
-			step++;
-		},time);
+		$tpa.hide(300,function(){
+			$(this).html(tpanHtm).show(300);
+		});
 	});
 	
 	/*下一步事件*/
 	$tpa.on("click",".next",function(){
 		switch(step){
+			case 1:
+				step1();
+			break;
 			case 2:
 				step2();
 			break;
 			case 3:
 				step3();
+			break;
+			case 4:
+				step4();
+			break;
+			case 5:
+				step5();
+			break;
+			case 6:
+				step6();
 			break;
 			default:
 			break;
@@ -51,22 +53,69 @@ $(function(){
 	});
 	
 	
+	
 	/**
-	*每一步的函数
+	*每一步的函数 每一步阐明理由最重要！！
 	*/
+	
+	/*第一步 创建空白页*/
+	function step1(){
+		$resume.html("<h1>我是空白页面</h1>").height("100vh");//生成空白页面
+		$typing.removeClass("col-md-offset-5 myhover");
+		$resume.removeClass("myhidden");
+		step++;
+	} 
 	
 	/*第二步 收缩空白页*/
 	function step2(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
 		$tpp.hide(300).html("第二步，我们按简历格式顺序塞入相关信息。").show(300);
-		$tpa.hide(300);
+		step++;
 	}
+	
 	/*第三步 显示简单简历*/
 	function step3(){
 		$resume.html(resHtm).height("auto");
 		$typing.removeClass("myhover");
 		$resume.removeClass("myhover");
+		step++;
+	}
+	
+	/*第四步 调整头像 列表项*/
+	function step4(){
+		$typing.addClass( "myhover");
+		$resume.addClass("myhover");
+		$tpp.hide(300).html("第三步，调整头像和列表项。").show(300);
+		step++;
+	}
+	
+	/*第五步 显示调整后的简历*/
+	function step5(){
+	
+		/*列表项去装饰*/
+		$("ul").addClass("list-unstyled");
+		
+		/*头像右浮动 信息左浮动*/
+		$(".main-bsinf").addClass("pull-left");
+		$(".avatar").addClass("pull-right");
+		
+		/*调整基本信息的列表项*/
+		$(".info-list").addClass("list-inline").width("300px");
+		$(".info-list li").css("margin-top",".5em");
+		$(".location").addClass("devider-right");
+
+		$typing.removeClass("myhover");
+		$resume.removeClass("myhover");
+		step++;
+	}
+	
+	/*第六步 调整间距 加入分隔符*/
+	function step6(){
+		$typing.addClass( "myhover");
+		$resume.addClass("myhover");
+		$tpp.hide(300).html("第四步，调整间距 加入分隔符。").show(300);
+		step++;
 	}
 	
 });
