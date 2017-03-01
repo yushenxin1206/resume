535b646c3654b3f07d79429c2898f7da880f7ed6
ztao <yushenxin@foxmail.com>
2017-03-01 21:38:29 +0800

    step13

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -39,6 +39,8 @@ $(function(){
 			case 5: step5(); break; case 6: step6(); break;
 		    case 7: step7(); break; case 8: step8(); break;
 			case 9: step9(); break; case 10: step10(); break;
+			case 11: step11(); break; case 12: step12(); break;
+			case 13: step13(); break; case 14: step14(); break;
 			default: break;
 		}
 	});
@@ -46,7 +48,7 @@ $(function(){
 	
 	
 	/**
-	*每一步的函数 每一步阐明理由最重要！！ 对着设计书看讲出理由
+	*每一步的函数 每一步阐明理由最重要！！ 对着设计书看讲出理由 最后可以给个对比
 	*/
 	
 	/*第一步 创建空白页*/
@@ -142,6 +144,8 @@ $(function(){
 		$(".heading").css("font-size","24px");
 		$("h3").css({"font-size":"16px","font-weight": "600"});
 		/*调整时间*/
+		$(".time").css({"color":"#778492","font-weight": "500"});
+		$("h3 .time").css({"font-weight": "600"});
 		
 		$typing.removeClass("myhover");
 		$resume.removeClass("myhover");
@@ -157,15 +161,56 @@ $(function(){
 	}
 	
 	/*第十一步 显示修改后的简历*/
-	function step10(){
-		$typing.addClass( "myhover");
-		$resume.addClass("myhover");
-		$tpp.hide(300).html("第六步，将个人技能提前。").show(300);
+	function step11(){
+		
+		var skillHtm='<div class="skills resume-parts">'+$(".skills").html()+'</div><hr class="divider"\
+					style="display: block;" />';
+		//注意顺序
+		$(".skills").prev("hr").remove();
+		$(".skills").remove();//必须在添加前remove 否则会把添加的移除
+		var a=$(".projects-exp").before(skillHtm);
+		
+		$typing.removeClass("myhover");
+		$resume.removeClass("myhover");
 		step++;
 	}
 	
-	/*第十二步 删除多余的元素*/
-	function step10(){
+	/*第十二步 美化个人技能部分*/
+	function step12(){
+		$typing.addClass( "myhover");
+		$resume.addClass("myhover");
+		$tpp.hide(300).html("第七步，美化个人技能部分。").show(300);
+		step++;
+	}
+	
+	/*第十三步 显示修改后的简历*/
+	function step13(){
+		
+		/*修改后的skill区域*/
+		var skillHtm='<h2 class="heading">个人技能</h2>\
+						<div id="stats-echarts" class="web-skills pull-left"></div>\
+						<div class="other-skills pull-right"><h3>其他技能</h3>\
+					<ul class="list-unstyled"><li>英语CET6</li><li>计算机四级</li>\
+					<li>熟悉图像处理算法</li></ul></div>';
+		$(".skills").html(skillHtm);
+		$(".skills h2").css({"margin":"0 0 20px","font-weight": "700","color": "#778492","font-size":"24px"});
+		
+		/*设置图区宽高*/
+		var width=$(".skills").width();
+		$("#stats-echarts").height(0.5*width);
+		$("#stats-echarts").width(0.6*width);
+		/*设置其他技能区宽高*/
+		$(".other-skills").height(0.5*width);
+		$(".other-skills").width(0.4*width-5);
+		/*显示图区*/
+		setEcharts();
+
+		$typing.removeClass("myhover");
+		$resume.removeClass("myhover");
+		step++;
+	}
+	/*第n步 删除多余的元素!!*/
+	function stepn(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
 		$tpp.hide(300).html("第六步，将个人技能提前。").show(300);
