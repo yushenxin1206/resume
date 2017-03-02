62c7197136cc2028bdacfb6ec2989f27ae20f740
ztao <yushenxin@foxmail.com>
2017-03-02 11:39:44 +0800

    step16

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -41,6 +41,7 @@ $(function(){
 			case 9: step9(); break; case 10: step10(); break;
 			case 11: step11(); break; case 12: step12(); break;
 			case 13: step13(); break; case 14: step14(); break;
+			case 15: step15(); break; case 16: step16(); break;
 			default: break;
 		}
 	});
@@ -63,7 +64,9 @@ $(function(){
 	function step2(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
-		$tpp.hide(300).html("第二步，我们按简历格式顺序塞入相关信息。").show(300);
+		$tpp.hide(300,function(){
+			$(this).html("第二步，我们按简历格式顺序塞入相关信息。").show(300);
+		});
 		step++;
 	}
 	
@@ -79,7 +82,9 @@ $(function(){
 	function step4(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
-		$tpp.hide(300).html("第三步，调整头像和列表项。").show(300);
+		$tpp.hide(300,function(){
+			$(this).html("第三步，调整头像和列表项。").show(300);
+		});
 		step++;
 	}
 	
@@ -107,7 +112,9 @@ $(function(){
 	function step6(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
-		$tpp.hide(300).html("第四步，调整间距 加入分隔符。").show(300);
+		$tpp.hide(300,function(){
+			$(this).html("第四步，调整间距 加入分隔符。").show(300);
+		});
 		step++;
 	}
 	
@@ -131,7 +138,9 @@ $(function(){
 	function step8(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
-		$tpp.hide(300).html("第五步，调整字体和颜色。").show(300);
+		$tpp.hide(300,function(){
+			$(this).html("第五步，调整字体和颜色。").show(300);
+		});
 		step++;
 	}
 	
@@ -156,7 +165,9 @@ $(function(){
 	function step10(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
-		$tpp.hide(300).html("第六步，将个人技能提前。").show(300);
+		$tpp.hide(300,function(){
+			$(this).html("第六步，将个人技能提前。").show(300);
+		});
 		step++;
 	}
 	
@@ -179,7 +190,9 @@ $(function(){
 	function step12(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
-		$tpp.hide(300).html("第七步，美化个人技能部分。").show(300);
+		$tpp.hide(300,function(){
+			$(this).html("第七步，美化个人技能部分。").show(300);
+		});
 		step++;
 	}
 	
@@ -190,7 +203,7 @@ $(function(){
 		var skillHtm='<h2 class="heading">个人技能</h2>\
 						<div id="stats-echarts" class="web-skills pull-left"></div>\
 						<div class="other-skills pull-right"><h3>其他技能</h3>\
-					<ul class="list-unstyled"><li>英语CET6</li><li>计算机四级</li>\
+					<ul class="list-unstyled"><li>英语CET6</li><li>计算机四级网络工程师</li>\
 					<li>熟悉图像处理算法</li></ul></div>';
 		$(".skills").html(skillHtm);
 		$(".skills h2").css({"margin":"0 0 20px","font-weight": "700","color": "#778492","font-size":"24px"});
@@ -209,11 +222,43 @@ $(function(){
 		$resume.removeClass("myhover");
 		step++;
 	}
-	/*第n步 删除多余的元素!!*/
-	function stepn(){
+	
+	/*第十四步 删除多余的元素 微调*/
+	function step14(){
 		$typing.addClass( "myhover");
 		$resume.addClass("myhover");
-		$tpp.hide(300).html("第六步，将个人技能提前。").show(300);
+		$tpp.hide(300,function(){
+			$(this).html("第八步，删除多余的元素 微调。").show(300);
+		});
+		step++;
+	}
+	
+	/*第十五步 显示修改后的简历*/
+	function step15(){
+		
+		/*删除标题*/
+		$("h1").remove();
+		
+		/*删除不必要的奖项*/
+		$(".sub-prizes:eq(0),.sub-prizes:eq(4)").remove();
+		
+		/*增加简历上下内边距*/
+		$(".resume").css("padding","60px 30px");
+		
+		/*姓名区下移*/
+		$(".main-bsinf").css("margin-top","10px");
+
+		$typing.removeClass("myhover");
+		$resume.removeClass("myhover");
+		
+		$(".next").html("完成");
+		step++;
+	}
+	
+	/*第十六步 终于完成*/
+	function step16(){
+		$(".typing").hide(300);
+		$(".resume").addClass("col-md-offset-2");
 		step++;
 	}
 	
